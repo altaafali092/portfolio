ef8ee1128004f381d7978d182585e1cd2c48a49f
altaafali092 <altaafali092@gmail.com>
2024-09-12 16:23:59 +0545

    commit

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -32,7 +32,7 @@ const Profile = () => {
                     <li className="flex items-center gap-x-2.5">
                         <svg className="shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2" /><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" /></svg>
                         <a className="text-[13px] text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400" href="#">
-                            elianagarcia997@about.me
+                            altaafali092@gmail.com
                         </a>
                     </li>
     
